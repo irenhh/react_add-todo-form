82e5a2d052ab8fb496c3713cb0f0c9595849768b
Iryna <iriskag@ukr.net>
2019-07-19 21:55:56 +0300

    Updates

---
+++ precache-manifest.99193d5a66ae44e6b2e18bbe9aeeaa45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e367d814caf51022ca534c97b961ec",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "e1fa247986bfa0957ae8",
+    "url": "/react_add-todo-form/static/css/main.83c2ace4.chunk.css"
+  },
+  {
+    "revision": "04d3a49fe037833a0b63",
+    "url": "/react_add-todo-form/static/js/2.97420267.chunk.js"
+  },
+  {
+    "revision": "e1fa247986bfa0957ae8",
+    "url": "/react_add-todo-form/static/js/main.6add0786.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
